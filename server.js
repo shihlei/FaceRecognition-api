ee5afcfa0425fe48b6b99272e17412b24f9ff87e
shihlei <shihlei121786@gmail.com>
2023-04-23 17:05:38 -0700

    modified DB connection info

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
+    connectionString : process.env.DATABASE_URL,
     host : process.env.DATABASE_HOST, //localhost
     user : process.env.DATABASE_USER,
     password : process.env.DATABASE_PW,
